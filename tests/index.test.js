4ff55297cef10b3f9ef00b37c0e1d4a2ba64f7cd
Karol <kpulawski13@gmail.com>
2024-04-14 14:37:18 +0100

    Add test for post /validate that validates user inputs

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,40 +1,49 @@
 const index = require('./index');
-
-const request = require('supertest');
 const express = require('express');
 const app = express();
+const request = require('supertest');
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', index);
 
-test('route /characters return json with characters', (done) => {
-  request(app)
-    .get('/characters')
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .then((res) => {
-      expect(res.body.characters).toHaveLength(3);
-      done();
-    })
-    .catch((err) => {
-      done(err);
-    });
+describe('GET /Characters', () => {
+  test('route /characters return json with characters', (done) => {
+    request(app)
+      .get('/characters')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.characters).toHaveLength(3);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 });
 
-test('POST /validate', (done) => {
-  request(app)
-    .post('/validate')
-    .send({ name: 'test1', xCoordinate: 100, yCoordinate: 200 })
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .then((res) => {
-      expect(res.body.message).toBeDefined();
-      done();
-    })
-    .catch((err) => {
-      done(err);
-    });
+describe('POST /validate', () => {
+  const userCharacterSelection = {
+    name: 'Bugs',
+    xCoordinate: 100,
+    yCoordinate: 200,
+  };
+
+  test('should validate character selection', (done) => {
+    request(app)
+      .post('/validate')
+      .type('form')
+      .send(userCharacterSelection)
+      .then((res) => {
+        expect(res.body.message).toMatch(
+          `That's right thats ${userCharacterSelection.name}`
+        );
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 });
