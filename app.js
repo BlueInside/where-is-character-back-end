82b90202ff5779ec865e6ece8982007d1a5f0ff4
Karol <kpulawski13@gmail.com>
2024-04-14 11:26:13 +0100

    Correct require path

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Connect to database
+require('./utils/database');
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-const indexRouter = require('./index');
+const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
 app.listen(3000, () => {
