dadf15d0eaea6d2af0b6aac86c3b6e5f34dcea8a
Karol <kpulawski13@gmail.com>
2024-04-14 12:17:52 +0100

    Get list of all characters

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
+const asyncHandler = require('express-async-handler');
+const Character = require('../models/Character');
+
 const express = require('express');
 const index = express.Router();
 
 const cords = [];
 
-index.get('/', (req, res) => {
-  res.json({ character: 'Karol' });
-});
+index.get(
+  '/characters',
+  asyncHandler(async (req, res) => {
+    const characters = await Character.find({}).sort({ name: -1 });
+
+    if (!characters)
+      return res.status(404).json({ error: 'Characters not found' });
+
+    res.json({ characters });
+  })
+);
 
 index.get('/coords', (req, res) => res.json({ cords }));
 
