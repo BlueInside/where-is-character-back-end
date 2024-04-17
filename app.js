e6e6a1cdbba525854e2801ac25d5e2ca53064412
Karol <kpulawski13@gmail.com>
2024-04-17 12:17:45 +0100

    Add new origin

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(helmet());
 app.use(express.static('public'));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://where-is-character.netlify.app',
     credentials: true,
   })
 );
