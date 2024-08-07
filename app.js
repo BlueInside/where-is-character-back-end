3ee23615e628c42386a61a5293810223b3bb3d06
Karol <kpulawski13@gmail.com>
2024-08-07 11:01:36 +0100

    remove session

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
 const compression = require('compression');
 const RateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -17,27 +15,10 @@ require('dotenv').config();
 
 // Middleware
 
-// Set up session
 
 // Enable trust for proxy headers
 app.set('trust proxy', 1);
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URL,
-      dbName: 'where_are_characters',
-    }),
-    cookie: {
-      maxAge: 2 * 24 * 3600000, // Two days
-      sameSite: 'none',
-      secure: process.env.NODE_ENV === 'production',
-    },
-  })
-);
 
 // Set up rate limiter: maximum of twenty requests per minute
 const limiter = RateLimit({
